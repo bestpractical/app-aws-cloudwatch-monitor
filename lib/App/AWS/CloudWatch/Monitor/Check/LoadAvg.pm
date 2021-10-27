cc0f2335c9565c7b3a395c677220a71a8e179680
Blaine Motsinger <blaine@bestpractical.com>
2021-10-26 19:29:13 -0500

    Add LoadAvg check module
    This check module gathers and returns metrics for:
    - LoadAvg1Min
    - LoadAvg5Min
    - LoadAvg15Min
    - LoadAvgPerCPU1Min
    - LoadAvgPerCPU5Min
    - LoadAvgPerCPU15Min

---
+++ lib/App/AWS/CloudWatch/Monitor/Check/LoadAvg.pm
@@ -0,0 +1,119 @@
+package App::AWS::CloudWatch::Monitor::Check::LoadAvg;
+
+use strict;
+use warnings;
+
+use parent 'App::AWS::CloudWatch::Monitor::Check';
+
+our $VERSION = '0.01';
+
+sub check {
+    my $self = shift;
+
+    my $loadavg_filename = '/proc/loadavg';
+    my ( $ret, $msg ) = $self->read_file($loadavg_filename);
+
+    if ( !$ret && $msg ) {
+        die "$msg\n";
+    }
+
+    return unless $ret;
+
+    my @fields = split /\s+/, @{$ret}[0];
+
+    # TODO: parse into float
+    my $loadavg_1min  = $fields[0];
+    my $loadavg_5min  = $fields[1];
+    my $loadavg_15min = $fields[2];
+
+    my $cpuinfo_filename = '/proc/cpuinfo';
+    ( $ret, $msg ) = $self->read_file($cpuinfo_filename);
+
+    if ( !$ret && $msg ) {
+        die "$msg\n";
+    }
+
+    return unless $ret;
+
+    my $cpu_count = grep {/processor\t:/} @{$ret};
+
+    my $metrics = [
+        {   MetricName => 'LoadAvg1Min',
+            Unit       => 'Percent',
+            RawValue   => $loadavg_1min,
+        },
+        {   MetricName => 'LoadAvg5Min',
+            Unit       => 'Percent',
+            RawValue   => $loadavg_5min,
+        },
+        {   MetricName => 'LoadAvg15Min',
+            Unit       => 'Percent',
+            RawValue   => $loadavg_15min,
+        },
+        {   MetricName => 'LoadAvgPerCPU1Min',
+            Unit       => 'Percent',
+            RawValue   => $loadavg_1min / $cpu_count,
+        },
+        {   MetricName => 'LoadAvgPerCPU5Min',
+            Unit       => 'Percent',
+            RawValue   => $loadavg_5min / $cpu_count,
+        },
+        {   MetricName => 'LoadAvgPerCPU15Min',
+            Unit       => 'Percent',
+            RawValue   => $loadavg_15min / $cpu_count,
+        },
+    ];
+
+    return $metrics;
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+App::AWS::CloudWatch::Monitor::Check::LoadAvg - gather load average metric data
+
+=head1 SYNOPSIS
+
+ my $plugin  = App::AWS::CloudWatch::Monitor::Check::LoadAvg->new();
+ my $metrics = $plugin->check();
+
+=head1 DESCRIPTION
+
+C<App::AWS::CloudWatch::Monitor::Check::LoadAvg> is a C<App::AWS::CloudWatch::Monitor::Check> module which gathers load average metric data.
+
+=head1 METRICS
+
+The following metrics are gathered and returned.
+
+=over
+
+=item LoadAvg1Min
+
+=item LoadAvg5Min
+
+=item LoadAvg15Min
+
+=item LoadAvgPerCPU1Min
+
+=item LoadAvgPerCPU5Min
+
+=item LoadAvgPerCPU15Min
+
+=back
+
+=head1 METHODS
+
+=over
+
+=item check
+
+Gathers the metric data and returns an arrayref of hashrefs with keys C<MetricName>, C<Unit>, and C<RawValue>.
+
+=back
+
+=cut
