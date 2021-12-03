ef835a97d1c4cc36013a9af697388b71b5568cd7
Blaine Motsinger <blaine@bestpractical.com>
2021-12-03 14:47:12 -0600

    Add NOTICE, LICENSE, and update copyright
    This commit adds the Apache 2.0 license for all new files, as well
    as updates the AWS files with license headers.

--- lib/App/AWS/CloudWatch/Monitor/CloudWatchClient.pm
+++ lib/App/AWS/CloudWatch/Monitor/CloudWatchClient.pm
@@ -1,3 +1,4 @@
+# Original file:
 # Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You may not
@@ -11,19 +12,20 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-# this package has been updated from the original version to:
-# Update formatting
-# Update package namespace and VERSION
-# Update open usage to modern Perl
-# Add explicit returns from subroutines
-# Avoid backtick operator in void context
-# Update subroutine called with "&" sigil
-# Update double sigil dereference
-# Update indirect object call syntax
-# Update reused variable name
-# Convert internal comments to pod
-# Add full namespace to AwsSignatureV4 calls
-# Fix undef warning on chomp in read_meta_data
+# Additional development:
+# Copyright 2021 Best Practical Solutions, LLC <sales@bestpractical.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 package App::AWS::CloudWatch::Monitor::CloudWatchClient;
 
